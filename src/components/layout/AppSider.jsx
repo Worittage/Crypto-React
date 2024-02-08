43e2197b4a1b6ac3da9cf965bf08c9136a40d735
Eugen <worittagebez@gmail.comssh-keygen -t rsa -b 4096 -C worittagebez@gmail.com>
2024-02-08 15:31:23 +0100

    addLoading

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,11 +1,45 @@
-import { Layout, Card, Statistic } from 'antd';
+import { Layout, Card, Statistic, List, Typography, Spin } from 'antd';
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import { fakeFetchCrypto, fetchAssets } from '../../api';
 
 const siderStyle = {
     padding: '1rem',
   };
 
+  const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];
+
   export default function AppSider() {
+    const [loading, setLoading] = useState(false)
+    const [crypto, setCrypto] = useState([])
+    const [assets, setAssets] = useState([])
+
+    useEffect(() => {
+      async function preload() {
+        setLoading(true)
+        const { result } = await fakeFetchCrypto()
+        const assets = await fetchAssets()
+
+        setAssets(assets)
+        setCrypto(result)
+        setLoading(false)
+      }
+      preload()
+    }, [])
+
+    if (loading) {
+      return <Spin fullscreen />
+      
+    }
+
+
+
     return (
         <Layout.Sider width="25%" style={siderStyle}>
           <Card style={{ marginBottom: '1rem' }}>
@@ -19,6 +53,15 @@ const siderStyle = {
             prefix={<ArrowUpOutlined />}
             suffix="%"
           />
+          <List
+          size='small'
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <Typography.Text mark>[ITEM]</Typography.Text> {item}
+        </List.Item>
+      )}
+    />
           </Card>
           <Card>
           <Statistic 
